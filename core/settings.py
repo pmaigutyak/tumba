1039de7ddb993567e7299006047682a5cbdbd803
sofiatkachuk1119 <sofiatkachuk1119@gmail.com>
2026-05-06 09:48:35 +0300

    change login system

--- core/settings.py
+++ core/settings.py
@@ -128,8 +128,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
